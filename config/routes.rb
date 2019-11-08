93e63729bca869c7da7ba6d0baa1de7c23b6feb7
Patrick Lyden <syncc12@verizon.net>
2019-11-08 20:16:36 +0000

    Backup to save add to database javascript and rails codes.  Other changes include moving the code from the moves views and controllers to the parties views and controllers.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
   resources :type_compares, only: :index
-  resources :moves, only: :index
-  resources :parties, only: :create
+  resources :parties #, only: [:index, :create]
 end
